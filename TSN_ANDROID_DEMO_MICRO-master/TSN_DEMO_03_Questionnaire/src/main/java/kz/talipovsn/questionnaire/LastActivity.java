1abaa2d9a748d3ef13402a0debb593b6d696f189
DanaOmarova <114468910+DanaOmarova@users.noreply.github.com>
2022-10-11 22:41:51 +0600

    Add files via upload

---
+++ TSN_ANDROID_DEMO_MICRO-master/TSN_DEMO_03_Questionnaire/src/main/java/kz/talipovsn/questionnaire/LastActivity.java
@@ -0,0 +1,26 @@
+package kz.talipovsn.questionnaire;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class LastActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_last);
+    }
+
+    // МЕТОД ДЛЯ КНОПКИ НАЗАД
+    public void onBack(View v) {
+        setResult(RESULT_OK);
+        finish();
+    }
+
+    // МЕТОД ДЛЯ КНОПКИ ВЫХОДА
+    public void onExit(View v) {
+        finishAffinity();
+    }
+}
